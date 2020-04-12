3c064063a4e3b77529a0a652b8d4d08253ee77e8
MichaelGreenburg <m.pgreenburg@gmail.com>
2020-04-12 00:58:41 -0500

    worked on more of the logic for the win, lose and draw functions

--- js/rockPaperScissors.js
+++ js/rockPaperScissors.js
@@ -1,22 +1,77 @@
-const userScore = 0;
-const computerScore = 0;
+let userScore = 0;
+let computerScore = 0;
 
 //caching the DOM//
 const userScore_span = document.getElementById("user-score");
 const computerScore_span = document.getElementById("computer-score");
 const scoreBoard_div = document.querySelector(".score-board");
-const result_div = document.querySelector(".result");
+const result_p = document.querySelector(".result > p");
 const rock_div = document.getElementById("r");
 const paper_div = document.getElementById("p");
 const scissors_div = document.getElementById("s");
 
 function getComputerChoice(){
-    const choices = ['r', 'p', 's'];
-    console.log(Math.random() * 3);
+    const choices = ['s', 'r', 'p'];
+    const randomNumber = Math.floor(Math.random() * 3);
+    return choices[randomNumber];
+}
+
+function convertToWord(letter){
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    return "Scissors";
+}
+
+function win(userChoice, computerChoice){
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = "user".fontsize(3).sub();//changes the user word to small text and puts it below the main text
+    const smallCompWord = "comp".fontsize(3).sub();//changes the comp word to small text and puts it below the main text
+    result_p.innerHTML = `${convertToWord(userChoice)}${smallUserWord}  beats ${convertToWord(computerChoice)}${smallCompWord}. You Win! `;
+
+}
+
+function lose(userChoice, computerChoice){
+    computerScore++;
+    computerScore_span.innerHTML = computerScore;
+    userScore_span.innerHTML = userScore;
+    const smallUserWord = "user".fontsize(3).sub();//changes the user word to small text and puts it below the main text
+    const smallCompWord = "comp".fontsize(3).sub();//changes the comp word to small text and puts it below the main text
+    result_p.innerHTML = `${convertToWord()}${smallUserWord} loses to ${convertToWord()}${smallCompWord}. You Lose!`
+}
+
+function draw(userChoice, computerChoice){
+    const smallUserWord = "user".fontsize(3).sub();//changes the user word to small text and puts it below the main text
+    const smallCompWord = "comp".fontsize(3).sub();//changes the comp word to small text and puts it below the main text
+    result_p.innerHTML = `${convertToWord()}${smallUserWord} equals ${convertToWord()}${smallCompWord}. It's A Draw!`
 }
 
 function game(userChoice) {
-    console.log(":poop :poop :poop" + userChoice);
+    // console.log(":poop :poop :poop" + userChoice);
+    const computerChoice = getComputerChoice();
+    // console.log(computerChoice); checks to make sure function works
+    // console.log("user choice => " + userChoice);
+    // console.log("computer choice => " + computerChoice);
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(userChoice, computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, computerChoice);
+            break;
+    }
+
+
 }
 
 function main() {
